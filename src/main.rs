cfb1b5666eaa6ce0ce66550b6eaa8d81b40f431a
Ngân Thành <lethanhngan544@gmail.com>
2025-09-29 16:42:33 +0700

    29/9

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,85 @@
-use gage::window;
-use gage::engine::types::{MessageQueue, EngineCvar};
+use gage::renderer;
+use gage::engine::types::{MessageQueue, EngineCvar, CVarValue, Message};
 use gage::colog;
+use gage::log;
+use gage::glfw;
 
-use std::sync::{Arc, Mutex};
 
+fn error_callback(error: glfw::Error, str: String) {
+    log::error!("GLFW error: {:?}, {}", error, str);
+}
 
 fn main() {
     colog::init();
 
+    let mut width = 1900;
+    let mut height = 600;
+    let title = "Sandbox !";
+
     let mut cvar = EngineCvar::new();
     let mut message_queue = MessageQueue::new();
-    let mut window = window::Window::new(
-        800,
-        600,
-        String::from("gage_sandbox")
-    );
+    cvar.insert(String::from("window_width"), CVarValue::Int(width as i32));
+    cvar.insert(String::from("window_height"), CVarValue::Int(height as i32));
+    cvar.insert(String::from("window_title"), CVarValue::Str(String::from(title)));
 
-    while window.running() {
-        window.update(&mut message_queue);
+    let mut glfw = glfw::init(error_callback).unwrap();
 
-        //Dispatch message
-        while let Some(event) = message_queue.pop_front() {
-            window.on_event(&event);
+    glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
+    let (mut window, reciever) = glfw.create_window(
+            width as u32,
+            height as u32,
+            title,
+            glfw::WindowMode::Windowed
+        ).unwrap();
+        window.set_all_polling(true);
+    
+    let mut renderer = renderer::Renderer::new(&window, &mut cvar);
+
+    
+    
+    while !window.should_close() {
+
+        //Update
+        {
+            glfw.poll_events();
+            //Reciever events and dispatch to message bus
+            for (_, event) in glfw::flush_messages(&reciever) {
+                match event {
+                    glfw::WindowEvent::CursorPos(x, y) => {
+                        message_queue.push_back(Message::MouseMoved(x as u32, y as u32));
+                    }
+                    glfw::WindowEvent::FramebufferSize(width, height) => {
+                        message_queue.push_back(Message::WindowResized(width as u32, height as u32));
+                    }
+                    glfw::WindowEvent::Key(Key, Scancode, Action, Modifiers) => {
+                        if Action == glfw::Action::Press {
+                            message_queue.push_back(Message::KeyPressed(Key as u32, Scancode as u32));
+                        } else if Action == glfw::Action::Release {
+                            message_queue.push_back(Message::KeyReleased(Key as u32, Scancode as u32));
+                        }
+                    }
+                    _ => ()
+                }
+            }
+
+            
+            //Dispatch message
+            while let Some(event) = message_queue.pop_front() {
+                renderer.on_message(event.clone());
+                if let Message::WindowResized(w, h) = event {
+                    width = w;
+                    height = h;
+                }
+            }
         }
+
+        //Render
+        {
+            renderer.render(&cvar);
+        }
+        
     }
+            
 }
+
+
